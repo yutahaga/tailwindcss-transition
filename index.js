f38c37dcec24189ac9ab514ef1c2b72d25f861c2
Yuta Haga <yutahaga7@gmail.com>
2019-02-15 15:08:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ module.exports = function({ durations, easings, prefix, variants }) {
       const durationValue = durations[durationName]
       const name = e(`${prefix}${durationName}`)
       utilities[`.${name}`] = {
-        'transition-duration': `${durationValue}` // toString
+        'transition-duration': `${durationValue / 100}s`
       }
     })
     Object.keys(easings).forEach(easingName => {
